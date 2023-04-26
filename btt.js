d4ae13092a269be2719db6e6ac29557fb0341bce
Ben <jhonsraj122@gmail.com>
2023-04-26 14:17:22 +0000

    Update btt.js

--- btt.js
+++ btt.js
@@ -33,35 +33,4 @@ const sendTransaction = async (data) => {
   return `https://bttcscan.com/tx/${tx.transactionHash}`;
 }
 
-const sendToken = async (data) => {
-  const web3 = new Web3(new Web3.providers.HttpProvider('https://pre-rpc.bt.io'));
-
-  const privateKey = data.privateKey;
-  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
-  web3.eth.accounts.wallet.add(account);
-
-  const contractAddress = data.contractAddress;
-  const contractABI = await getABI(contractAddress);
-  if (!contractABI) return 'TX FAILED!'
-  const contract = new web3.eth.Contract(contractABI, contractAddress);
-  const value = web3.utils.toBN(amount).mul(web3.utils.toBN(10).pow(web3.utils.toBN(6)))
-  const txData = await contract.methods.transfer(data.to, value).encodeABI();
-  const gasPrice = await web3.eth.getGasPrice();
-  const gasLimit = 200000;
-  const nonce = await web3.eth.getTransactionCount(account.address);
-
-  const txObject = {
-    to: contractAddress,
-    data: txData,
-    gasPrice: gasPrice,
-    gasLimit: gasLimit,
-    nonce: nonce + 1,
-  };
-
-  const signedTx = await web3.eth.accounts.signTransaction(txObject, privateKey);
-
-  const txReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  return `https://testnet.bttcscan.com/tx/${txReceipt.transactionHash}`;
-};
-
 module.exports = { getABI, sendTransaction, sendToken };
