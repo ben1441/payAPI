fc64ab0971691e34a8080226af1c62e0794907b3
Ben <jhonsraj122@gmail.com>
2023-04-26 14:28:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const btt = require('./btt');
+const matic = require('./matic');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -7,6 +8,14 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('server up and running!')
+});
+
+app.post('/matic/:op', async (req, res) => {
+  try {
+    res.status(200).send(await matic[req.params.op](req.body))
+  } catch(e) {
+    res.status(500).send(e || 'oops... An error occurred!')
+  }
 });
 
 app.post('/btt/:op', async (req, res) => {
